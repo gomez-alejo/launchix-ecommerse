ce4d090839995ad6662b004d2f577ad099c82e15
gomez-alejo <alejito15c@gmail.com>
2025-10-10 11:39:47 -0500

    creacion de home

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-// Página principal
-Route::get('/', function () {
-    return view('welcome');
-});
 
-
-// Listar productos
-Route::get('/productos', [ProductController::class, 'index'])->name('productos.index');
-
-// Mostrar detalle de un producto
-Route::get('/productos/{id}', [ProductController::class, 'show'])->name('productos.show');
-
-// Mostrar formulario de edición de un producto
-Route::get('/productos/{id}/edit', [ProductController::class, 'edit'])->name('productos.edit');
 
